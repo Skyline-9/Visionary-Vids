47413170c8f2137948a783e8dc9db0c7a7275b20
Richard Luo <luo.richard@gmail.com>
2023-04-22 22:31:59 -0400

    Added script to download qvhighlights

---
+++ datasets/download_qvhighlights.sh
@@ -0,0 +1,2 @@
+wget --content-disposition https://connectpolyu-my.sharepoint.com/:u:/g/personal/21039533r_connect_polyu_hk/EVyfPQmNEfxCpvWO3Lp-6NkBld4GHGH8sPj1ZVkx4ScKNg?download=1
+unzip -d ../data qvhighlights-a8559488.zip
