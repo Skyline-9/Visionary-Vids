6acabc534a4aff7ea298a1265444f971445e8674
Koby Beard <kobybeard@gmail.com>
2023-04-26 19:11:44 -0400

    Added Lion Optimizer

--- configs/_base_/schedules/100e.py
+++ configs/_base_/schedules/100e.py
@@ -1,6 +1,9 @@
 # runtime settings
+
+from lion import Lion
+
 stages = dict(
     epochs=100,
-    optimizer=dict(type="Adam", lr=1e-3, weight_decay=1e-4),
+    optimizer=dict(type="Lion", lr=1e-3, weight_decay=1e-4),
     validation=dict(interval=1),
 )
